e5bf175160beb741a43837e9869a0747ef1dbaa6
Gabriel.s <gabrielsantosayres@gmail.com>
2023-09-05 16:20:06 -0300

    Revisando exercicios

---
+++ praticandoArray/script.js
@@ -0,0 +1,19 @@
+const numeros = [10, 17, 8, 23, 56, 92, 33, 70, 19, 5, 61, 88, 12, 99, 27, 39, 53, 2, 77, 50, 3, 67, 29, 14, 72, 6, 91, 46, 10, 81, 38, 60, 22, 49, 1, 69, 25, 9, 75, 41, 18, 87, 35, 68, 13, 64, 30, 7, 48, 26, 84, 55, 31, 4, 80, 21, 73, 34, 58, 15, 71, 59, 11, 45, 20, 76, 37, 65, 24, 90, 51, 28, 78, 43, 16, 82, 32, 66, 36, 74, 47, 63, 40, 85, 52, 89, 54, 86, 57, 44, 83, 62, 94, 95, 98, 100, 96, 97];
+
+/*const pares = numeros.filter((valor, indice)=> {
+    if(valor%2 ===0){
+        return console.log(`${indice} => ${valor}`);
+    }
+})*/
+
+/*const impares = numeros.filter((valor, indice)=>{
+    if(valor%2 ===  1){
+        return console.log(`Indice: ${indice} => ${valor}`);
+    }
+})*/
+const somaTotal = numeros.reduce((ac, valor) => {
+    return ac += valor;
+}, 0);
+
+console.log();
+console.log(somaTotal);
---
+++ praticandoClasses/simulandoBanco.js
@@ -0,0 +1,91 @@
+class ContaBancaria{
+
+    constructor(conta, saldo){
+        this.conta = conta;
+        this.saldo = 0;
+    }
+
+    sacar(valor){
+        if(valor <= this.saldo){
+            this.saldo -= valor;
+            return console.log(`${conta}/ Valor Sacado R$${valor}/ Saldo total R$${this.saldo.toFixed(2)}`);
+        }
+        if(valor > this.saldo){
+            return console.log("Saldo Insuficiente! realize um deposito")
+        }
+
+    }
+
+    depositar(valor){ 
+        this.saldo += valor;
+        return //console.log(`Saldo  atualizado R$:${this.saldo.toFixed(2)}`);
+    }
+
+    verSaldo(){
+        return console.log(`Conta: ${this.conta} Saldo: ${this.saldo.toFixed(2)}`);
+    }
+
+
+
+}
+
+/*const conta1 = new ContaBancaria(1221, 1);
+console.log(conta1);
+conta1.depositar(1000);
+conta1.verSaldo();
+conta1.sacar(1200);
+conta1.verSaldo();*/
+
+const limite = Symbol('limite');//é uma forma de proteger o código, tornando o atributo _limite mais privado e menos acessível fora da classe.
+// conta corrente com limite
+class ContaCorrente extends ContaBancaria {
+    constructor(conta, saldo, limite){
+        super(conta, saldo);//super faz a referencia dos atributos da superclasse
+        this.limite = 0;
+    }
+
+    getLimite(){
+        return console.log(`O seu limite é de R$:${this.limite.toFixed(2)}`);
+    }
+    
+    setLimite(valorLimite){
+        this._limite = valorLimite;
+    }
+
+    sacar(valor){
+        if(valor <= (this.saldo + this.limite)){
+            if(valor < this.saldo){
+                this.saldo -= valor;
+            return console.log(`${this.conta}/ Valor Sacado R$${valor}/ Saldo total R$${this.saldo.toFixed(2)}`);
+            }else if(valor > this.saldo){
+                const valorTotal =  (valor - this.saldo);
+                this.saldo = 0;
+                this.limite -= valorTotal;
+                return console.log(`${this.conta}/ Valor Sacado R$${valor}/ Saldo total R$${this.saldo.toFixed(2)}/ Limite restante R$:${this.limite.toFixed(2)}`);
+            }
+        }// se o saque for maior que o saldo ele utiliza o saldo + o limite
+        if(valor > (this.saldo + this.limite)){
+            return console.log("Saldo Insuficiente! realize um deposito")
+        }
+
+    }
+}
+
+//const conta2 = new ContaCorrente(3399);
+//conta2.depositar(1000);
+//conta2.limite = 1000;
+//conta2.sacar(1500);
+//console.log(conta2);
+//conta2.sacar(1500);
+//conta2.verSaldo();
+
+class ContaPoupanca extends ContaBancaria{
+    constructor(conta, saldo){
+        super(conta, saldo);
+    }
+}
+
+const poup = new ContaPoupanca(3301);
+poup.depositar(10000);
+poup.verSaldo();
+poup.sacar(10000.01);
