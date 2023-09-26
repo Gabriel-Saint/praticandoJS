263420502f1b1055f9f0f57807c993a37472178f
Gabriel.s <gabrielsantosayres@gmail.com>
2023-09-26 16:56:53 -0300

    Helmet

--- CRUD/server.js
+++ CRUD/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+//const port = 3000;
 const session =  require('express-session');
+const helmet = require('helmet');
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(helmet());
 
 
 app.use(express.static('view'));
@@ -26,8 +28,6 @@ app.use(session({
 const routes = require('./config/routes');
 
 app.use('/', routes);
-
-
 
 app.listen(port, ()=> {
     console.log(`Servidor rodando em http://localhost:${port}`);
